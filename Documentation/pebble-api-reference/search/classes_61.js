594bf1468b5816cd5ec8520c2e1842247a0096d7
Andrei Gherzan <andrei.gherzan@windriver.com>
2014-01-07 11:32:47 +0200

    Add PebbleSDK 2.0-BETA
    Fix requirements as stated in:
    https://developer.getpebble.com/2/getting-started/linux/

---
+++ Documentation/pebble-api-reference/search/classes_61.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['animationhandlers',['AnimationHandlers',['../group___animation.html#struct_animation_handlers',1,'']]],
+  ['animationimplementation',['AnimationImplementation',['../group___animation.html#struct_animation_implementation',1,'']]]
+];
